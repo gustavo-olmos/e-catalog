8b69dcc0648bba79ee257b2ba0081cd92769d91f
Gustavo <guolmos06@gmail.com>
2025-09-15 00:17:05 -0300

    Adiciona componentes e páginas para e-catalog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeadComponent } from './components/head/head.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    PortfolioComponent,
+    LandingComponent,
+    GalleryComponent,
+    ProfileComponent,
+    FooterComponent,
+    SidebarComponent,
+    HeadComponent
   ],
   imports: [
     BrowserModule,
